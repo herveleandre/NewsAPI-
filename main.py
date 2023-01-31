ef0f509f6ccfb6a02adf763a7f40391af70ed447
herveleandre <47299858+herveleandre@users.noreply.github.com>
2023-01-31 06:23:50 -0500

    changed from date to current. Complete

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 from send_email import send_email
 
+
+topic = "tesla"
 api_key = "7a90230871614d94acae52a2bc13172c"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2022-12-29&sortBy=publishedAt&apiKey" \
-      "=7a90230871614d94acae52a2bc13172c"
+# Ensure from date is current or a month behind
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2023-1-29&sortBy=publishedAt&apiKey" \
+      "=7a90230871614d94acae52a2bc13172c&language=en"
 
 # Make request
 request = requests.get(url)
@@ -12,9 +15,10 @@ content = request.json()
 
 # Access article's title, author, ID and description.
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article is not None:
-        body = body + article['title'] + "\n" + article['description'] + 2 * "\n"
+        body = "Subject: Today's new by Herve" + "\n" + body + article['title'] + "\n" + \
+               article['description'] + "\n" + article["url"] + 2 * "\n"
 
 body = body.encode("utf-8")
 send_email(body)
